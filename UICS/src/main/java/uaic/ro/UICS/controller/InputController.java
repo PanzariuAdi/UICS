de4a94069d0ae639c8761ddb46baa572bb97b660
BirzuCosminConstantin <birzu.cosmin.c@gmail.com>
2024-01-31 18:40:04 +0200

    Change API get

--- UICS/src/main/java/uaic/ro/UICS/controller/InputController.java
+++ UICS/src/main/java/uaic/ro/UICS/controller/InputController.java
@@ -3,6 +3,7 @@ package uaic.ro.UICS.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uaic.ro.UICS.languageProcessor.Node;
 import uaic.ro.UICS.services.LanguageService;
@@ -14,7 +15,7 @@ public class InputController {
     LanguageService languageService;
 
     @GetMapping("/")
-    public Node getAllNodes(@RequestBody String input) {
+    public Node getAllNodes(@RequestParam String input) {
         return languageService.getNodes(input);
     }
 
